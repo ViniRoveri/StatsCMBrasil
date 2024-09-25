01de062c6bafc9a79437424d4ae4b15fc4a362bc
Vinícius Roveri <viniciusroverirocha@gmail.com>
2024-09-25 16:17:46 -0300

    Spell fix & header fix

--- frontend/src/domain/constants/homePageItems.ts
+++ frontend/src/domain/constants/homePageItems.ts
@@ -1,12 +1,12 @@
 const homePageItems = [
    {
       url: '/maiores/historia/333',
-      title: 'Maiores História', 
+      title: 'Maiores da História', 
       text: 'Os rankings de maiores da história de cada categoria, baseado em cálculos sobre resultados, recordes, e performances em campeonatos!'
    },
    {
       url: '/maiores/ano/333',
-      title: 'Maiores Ano', 
+      title: 'Maiores do Ano', 
       text: 'O ranking da atualidade sempre atualizado com quem vem dominando o cenário do esporte em cada categoria!'
    },
    {
