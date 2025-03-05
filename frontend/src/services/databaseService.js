be55bb636197ba6a4eb3d0786850fa91a6d5c1e3
Vinícius Roveri <viniciusroverirocha@gmail.com>
2025-03-05 11:48:32 -0300

    05/03/2025

--- frontend/src/services/databaseService.js
+++ frontend/src/services/databaseService.js
@@ -3,37 +3,37 @@ import fs from 'fs'
 const databaseService = {
    getCampeoesByEvent(event){
       return JSON.parse(
-         fs.readFileSync(`./src/database/campeoes/${event}.json`)
+         fs.readFileSync(process.cwd() + `/src/database/campeoes/${event}.json`)
       )
    },
 
    getMaioresAnoWinners(){
       return JSON.parse(
-         fs.readFileSync('./src/database/maioresAno/winners.json')
+         fs.readFileSync(process.cwd() + '/src/database/maioresAno/winners.json')
       )
    },
    
    getMaioresAnoByYearAndEvent(year, event){
       return JSON.parse(
-         fs.readFileSync(`./src/database/maioresAno/${year}/${event}.json`)
+         fs.readFileSync(process.cwd() + `/src/database/maioresAno/${year}/${event}.json`)
       )
    },
 
    getMaioresCampeoesByEvent(event){
       return JSON.parse(
-         fs.readFileSync(`./src/database/campeoes/maiores/${event}.json`)
+         fs.readFileSync(process.cwd() + `/src/database/campeoes/maiores/${event}.json`)
       )
    },
 
    getMaioresHistoriaByEvent(event){
       return JSON.parse(
-         fs.readFileSync(`./src/database/maioresHistoria/${event}.json`)
+         fs.readFileSync(process.cwd() + `/src/database/maioresHistoria/${event}.json`)
       )
    },
    
    getRecordesByTypeAndEvent(type, event){
       return JSON.parse(
-         fs.readFileSync(`./src/database/recordes/${type}/${event}.json`)
+         fs.readFileSync(process.cwd() + `/src/database/recordes/${type}/${event}.json`)
       )
    },
 }
