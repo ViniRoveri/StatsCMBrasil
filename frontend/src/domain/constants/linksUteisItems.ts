1b3a227a2f3352c029ba8109eb5615747d7c87c3
Vinícius Roveri <viniciusroverirocha@gmail.com>
2025-06-13 09:30:22 -0300

    13/06/2025

--- frontend/src/domain/constants/linksUteisItems.ts
+++ frontend/src/domain/constants/linksUteisItems.ts
@@ -23,7 +23,17 @@ const linksUteisItems = [
       url: '/regraCalculoMaiores',
       title: 'Regra de cálculo de "Maiores"', 
       text: 'Como são feitos os cálculos do ranking de Maiores da História e Maiores do Ano aqui do site!'
-   }
+   },
+   {
+      url: 'https://www.instagram.com/vini_roveri',
+      title: 'Instagram do Vini', 
+      text: 'Se quiser fortalecer o criador deste acervo, ou quiser um contato direto comigo, fique à vontade pra me seguir lá no Instagram!'
+   },
+   {
+      url: 'https://www.tiktok.com/@vini_roveri',
+      title: 'TikTok do Vini', 
+      text: 'E se quiser fortalecer mais ainda o criador deste acervo me segue lá no TikTok pra acompanhar os melhores vídeos de cubo mágico do planeta!'
+   },
 ]
 
 export default linksUteisItems
